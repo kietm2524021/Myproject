2e6f0b51044b3f83dc2183eec5e914bad6f1b04a
kietm2524021 <kietm2524021@gstudent.ctu.edu.vn>
2025-03-22 09:47:50 +0700

    new web

--- app.js
+++ app.js
@@ -1,23 +1,64 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
-
+const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    // Đọc file index.html
-    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
-        if (err) {
-            res.writeHead(500, { "Content-Type": "text/plain" });
-            res.end("Lỗi server!");
-        } else {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-        }
-    });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
+    <!DOCTYPE html>
+    <html lang="vi">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bảng Thông Tin</title>
+        <style>
+            table {
+                width: 100%;
+                border-collapse: collapse;
+            }
+            th, td {
+                border: 1px solid black;
+                padding: 8px;
+                text-align: left;
+            }
+        </style>
+    </head>
+    <body>
+
+    
+
+    <table>
+        <tr>
+            <th>MSHV</th>
+            <th>Họ tên</th>
+            <th>Ngày sinh</th>
+            <th>Quê quán</th>
+            <th>Email</th>
+            <th>SĐT</th>
+        </tr>
+        <tr>
+            <td>M2524021</td>
+            <td>Huỳnh Tuấn Kiệt</td>
+            <td>01/01/2002</td>
+            <td>Bình Dương</td>
+            <td>k@gmail.com</td>
+            <td>0756378312</td>
+        </tr>
+        <tr>
+        <td>M2524024</td>
+        <td>Vương Cẩm Thanh</td>
+        <td>01/01/2002</td>
+        <td>Bình Dương</td>
+        <td>t@gmail.com</td>
+        <td>0756378312</td>
+        </tr>
+    </table>
+
+    </body>
+    </html>
+  `);
 });
 
 server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at port ` + port);
 });
-
